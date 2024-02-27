0be65b1ac3f77b4e2d6a0f77e29d55b7546edd32
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-26 22:07:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a positive integer: ");
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number) || number <= 0)
+        {
+            Console.WriteLine("Please enter a valid positive integer: ");
+        }
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
